11e7d19b9cede364407d2a65a79e640d36c2e6f3
jamesb12 <james@squeezetech.com>
2016-08-05 11:49:04 -0700

    pushing some stuf

---
+++ db/migrate/20160804162624_create_jobs.rb
@@ -0,0 +1,12 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string :customer
+      t.string :project
+      t.decimal :hours
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
