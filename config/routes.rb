9f0f5467d0786fbc56444c7cdadc5cda4e146e01
jamesb12 <james@squeezetech.com>
2016-08-05 11:50:36 -0700

    yala

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'jobs/new'
+  
 
 	get 'signup', to: 'users#new', as: 'signup'
 	get 'login', to: 'sessions#new', as: 'login'
 	get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  get 'home/index'
-
+ get 'home/index'
+resources :jobs
   resources :users
   resources :sessions
   root 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729211814) do
+ActiveRecord::Schema.define(version: 20160804162624) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "customer"
+    t.string   "project"
+    t.decimal  "hours"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
