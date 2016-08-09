1afae042e97ea7ee6204a076a1c96a700155e41c
jamesb12 <james@squeezetech.com>
2016-08-09 12:02:45 -0700

    initial progress on admin page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805210644) do
+ActiveRecord::Schema.define(version: 20160808163915) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "customer"
