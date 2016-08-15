24f147cf1365b79e8806d8b086f0c54053f5da8b
jamesb12 <james@squeezetech.com>
2016-08-15 10:01:03 -0700

    test reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808163915) do
+ActiveRecord::Schema.define(version: 20160815165059) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20160808163915) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "date"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
