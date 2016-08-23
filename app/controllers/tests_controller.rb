20dafe2598a260ff1d147d46bdee645f6b03044b
jamesb12 <james@squeezetech.com>
2016-08-23 11:48:43 -0700

    adding alot of syle/bootstrap and fixing various bugs

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -28,7 +28,7 @@ class TestsController < ApplicationController
 
     respond_to do |format|
       if @test.save
-        format.html { redirect_to @test, notice: 'Test was successfully created.' }
+        format.html { redirect_to @test }
         format.json { render :show, status: :created, location: @test }
       else
         format.html { render :new }
