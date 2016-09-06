f0bb6801909c518018f14c0b13a872a1e0e869bf
james3389 <james@squeezetech.com>
2016-09-06 13:35:53 +0200

    making work with sqlite for coursework

--- db/schema.rb
+++ db/schema.rb
@@ -12,58 +12,22 @@
 
 ActiveRecord::Schema.define(version: 20160822191151) do
 
-  create_table "clients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "jobs", force: :cascade do |t|
     t.string   "customer"
     t.string   "project"
-    t.decimal  "hours",                     precision: 10
-    t.text     "description", limit: 65535
+    t.decimal  "hours"
+    t.text     "description"
     t.string   "date"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-  end
-
-  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "roll_calls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-  end
-
-  create_table "rolls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.integer  "server_id"
-    t.string   "notes"
-    t.string   "status",       default: "Unchecked"
-    t.integer  "roll_call_id"
-    t.string   "server_name"
-    t.string   "client_name"
-    t.index ["roll_call_id"], name: "index_rolls_on_roll_call_id", using: :btree
-    t.index ["server_id"], name: "index_rolls_on_server_id", using: :btree
-  end
-
-  create_table "servers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name"
-    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "client_id"
-    t.index ["client_id"], name: "index_servers_on_client_id", using: :btree
   end
 
-  create_table "sorts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "reports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sorts", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
     t.string   "user"
@@ -71,24 +35,21 @@ ActiveRecord::Schema.define(version: 20160822191151) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "tests", force: :cascade do |t|
     t.date     "date_1"
     t.date     "date_2"
     t.string   "name"
     t.string   "user"
-    t.decimal  "hours",      precision: 10
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.decimal  "hours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "rolls", "roll_calls"
-  add_foreign_key "rolls", "servers"
-  add_foreign_key "servers", "clients"
 end
